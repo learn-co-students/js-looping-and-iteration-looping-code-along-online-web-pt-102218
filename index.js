7967709cf6cc5568e8256c9522cf28c2ec7ec3c1
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-21 15:45:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const names = ["Ada", "Bren"];
+const events = "birthday";
+
+function writeCards(names, events) {
+  let thankyou = []; 
+  for (let i = 0; i < names.length; i++) {
+    thankyou.push('Thank you, ${names[i]}, for the wonderful ${events}');
+  }
+  return thankyou
+}
+
+function countdown(countdown) {
+  while (countdown > 0) {
+    console.log(countdown)
+    countdown -=1
+  }
+  console.log(countdown)
+}
