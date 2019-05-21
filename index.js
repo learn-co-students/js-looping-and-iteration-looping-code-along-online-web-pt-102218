e970079fe2750417ced6111bb39503c8e7c7d3a0
Johnathon Wood <jaywood128@gmail.com>
2019-05-21 16:42:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thanks = [];
+    for (let i= 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks 
+}   
+function countdown(countdown) {
+    while (countdown > 0) {
+        console.log(countdown);
+        countdown -= 1;
+    }
+    console.log(countdown)
+}
