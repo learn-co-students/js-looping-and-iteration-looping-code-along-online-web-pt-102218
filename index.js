ce63a5e7877acf05308b1eb3ccc9585303124fe9
Matthew McCormack <mmccormack1182@gmail.com>
2019-05-22 09:04:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(namesList, eventName) {
+  let messages = []
+  for (let i = 0; i < namesList.length; i++) {
+    messages.push(`Thank you, ${namesList[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return messages
+}
+
+
+function countdown() {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i)
+    i--
+  }
+}
