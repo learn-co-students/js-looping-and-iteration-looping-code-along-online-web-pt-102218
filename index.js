e4d74125372ddc8856c3d77e2bca4518ca293f84
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-27 18:26:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGifts(gifts) {
+  let i = 0; // the initialization moved OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+ 
+  return gifts;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function plantGarden() {
+  let keepWorking = true;
+  while (keepWorking) {
+    chooseSeedLocation();
+    plantSeed();
+    waterSeed();
+    keepWorking = checkForMoreSeeds();
+  }
+}
