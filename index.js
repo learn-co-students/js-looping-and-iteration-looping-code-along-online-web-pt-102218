4bbd5223e08ab0ea72a9685883ea18b2ebfdd473
Paul Ashour <paul.ashour@gmail.com>
2019-06-01 17:58:45 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array) {
+  const retArr = []
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    retArr.push(`Thank you, ${element}, for the wonderful surprise gift!`)
+  }
+  return retArr
+};
+
+function countdown(num) {
+  for (let index = num; index >= 0; index--) {
+    console.log(index)
+    
+  }
+}
